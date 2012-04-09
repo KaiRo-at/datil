c51b5bed43bb72febfa893fe1d5573769efe7f64
Robert Kaiser <kairo@kairo.at>
2012-04-09 17:51:59 +0200

    switch dashboard to 12.0b4

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -30,7 +30,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "12.0b3",
+        version: "12.0b4",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
@@ -76,7 +76,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "12.0b3",
+        version: "12.0b4",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 2.5 },
