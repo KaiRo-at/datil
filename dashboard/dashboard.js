a692b1d07859b646f04768e586eb2a7e3ee920cd
Robert Kaiser <kairo@kairo.at>
2012-05-02 15:50:48 +0200

    add 13.0b2, prepare for native beta (14)

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -118,7 +118,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "13.0b1",
+        version: "14.0b1",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 2.5 },
