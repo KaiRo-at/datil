3afdded6a189c3c50d7d949d319147d0238d9962
Robert Kaiser <kairo@kairo.at>
2012-05-29 15:10:40 +0200

    update mincounts, add 13.0b6, switch dashboard to newer betas

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -30,7 +30,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "13.0b4",
+        version: "13.0b5",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
@@ -118,7 +118,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "14.0b2",
+        version: "14.0b3",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 5 },
