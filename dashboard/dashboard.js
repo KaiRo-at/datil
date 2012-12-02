5542a1102fa3dc48ed701fad17687d24da621670
Robert Kaiser <kairo@kairo.at>
2012-12-02 16:06:43 +0100

    switch dashboard to 18.0b2 and FF 17.0.1

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -32,7 +32,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "18.0b1",
+        version: "18.0b2",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
@@ -44,7 +44,7 @@ var gProductData = {
       },
       release: {
         name: "Release",
-        version: "17.0",
+        version: "17.0.1",
         appendver: true,
         adu: { low: 1e8, min: 1e7 },
         rate: { factor: 1, high: 2, max: 2.5 },
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "18.0b1",
+        version: "18.0b2",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 5 },
