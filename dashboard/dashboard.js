0aafc9430884bd294a564987fcbf9d475d2748b9
Robert Kaiser <kairo@kairo.at>
2013-02-15 15:36:49 +0100

    bump dashboard to b6

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -32,7 +32,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "19.0b5",
+        version: "19.0b6",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "19.0b5",
+        version: "19.0b6",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 5 },
