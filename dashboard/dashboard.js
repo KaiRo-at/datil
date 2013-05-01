a6426122eafcb91155e12a3be4a4cd43f3a25218
Robert Kaiser <kairo@kairo.at>
2013-05-01 16:19:02 +0200

    remove old Flash beta, bump dashboard to desktop b5

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -32,7 +32,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "21.0b4",
+        version: "21.0b5",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
