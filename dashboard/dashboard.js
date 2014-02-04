149145c8a3fadc29e661588b8003eb8621b37bc6
Robert Kaiser <kairo@kairo.at>
2014-02-04 15:28:49 +0100

    add new versions coming on this week; bump dashboard to b99 for the moment

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -32,7 +32,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "27.0b",
+        version: "27.0b99",
         appendver: true,
         adu: { low: 1e7, min: 1e6 },
         rate: { high: 2, max: 2.5 },
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "27.0b9",
+        version: "27.0b99",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 3 },
