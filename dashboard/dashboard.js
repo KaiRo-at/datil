1ad46e9a832717cebfd0e98d532d2b41ecdabff9
Robert Kaiser <kairo@kairo.at>
2014-05-12 16:48:50 +0200

    version and mincount adjustments

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "30.0b1",
+        version: "30.0b2",
         appendver: true,
         adu: { low: 1e5, min: 1e4 },
         rate: { high: 2, max: 3 },
