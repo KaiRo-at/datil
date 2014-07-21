742f6abb298b903a39f1d4af3d1d9a0cb7d6e2c9
Robert Kaiser <kairo@kairo.at>
2014-07-21 18:30:02 +0200

    bump dashboard to b11

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "31.0b10",
+        version: "31.0b11",
         appendver: true,
         adu: { low: 6e4, min: 3e4 },
         rate: { high: 2, max: 2.5 },
