6922079202d74d70c3d7a84cddec77bff0899797
Robert Kaiser <kairo@kairo.at>
2015-05-01 17:15:47 +0200

    version fixes

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "38.0b6",
+        version: "38.0b8",
         appendver: true,
         adu: { low: 6e4, min: 3e4 },
         rate: { high: 2, max: 2.5 },
