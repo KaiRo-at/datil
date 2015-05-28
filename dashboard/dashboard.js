4f9f7f84054aa74b3d5b8ad20c23a21ce3a35aa3
Robert Kaiser <kairo@kairo.at>
2015-05-28 18:53:35 +0200

    Android released a beta for 38.0.5

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "38.0b10",
+        version: "38.0.5b4",
         appendver: true,
         adu: { low: 6e4, min: 3e4 },
         rate: { high: 2, max: 2.5 },
