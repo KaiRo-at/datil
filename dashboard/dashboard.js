2f2de2e48d86bb3188c1f9afa223065f0e459e06
Robert Kaiser <kairo@kairo.at>
2015-06-10 12:10:58 +0200

    put android b2 on dashboard

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "39.0b1",
+        version: "39.0b2",
         appendver: true,
         adu: { low: 6e4, min: 3e4 },
         rate: { high: 2, max: 2.5 },
