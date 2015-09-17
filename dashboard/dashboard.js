31ca7873d2aa2aa6cbbc087f17839cc3fe253f72
Robert Kaiser <kairo@kairo.at>
2015-09-17 16:18:27 +0200

    update for versions coming next week

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -82,7 +82,7 @@ var gProductData = {
       },
       beta: {
         name: "Beta",
-        version: "41.0b6",
+        version: "41.0b10",
         appendver: true,
         adu: { low: 6e4, min: 3e4 },
         rate: { high: 2, max: 2.5 },
